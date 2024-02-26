ba878298d8de8adeb67ad2b3360754901e71fbe4
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-02-26 10:35:28 -0300

    feat: add new logotype

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,12 +1,12 @@
 @if(Route::current()->getName() === 'filament.admin.auth.login')
-    <img src="{{asset('image/readme/logo479x118.png')}}"
+    <img src="{{asset('image/readme/corporatix-logo-h-380.png')}}"
          alt="{{config('app.name')}}"
          title="{{config('app.name')}}"
          width="310"
          class="mb-2"
     >
 @else
-    <img src="{{asset('image/readme/logo479x118.png')}}"
+    <img src="{{asset('image/readme/corporatix-logo-h-380.png')}}"
          alt="{{config('app.name')}}"
          title="{{config('app.name')}}"
          width="165"
